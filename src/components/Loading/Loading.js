1c60aab37c743aa5b2914fe760c9b25427cf1fd3
Carlos Castillo <info@carloscastillo.cl>
2020-06-21 00:49:43 +0100

    Added Loading animation component for Latest comic

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Loading.module.scss';
+
+const Loading = () => {
+    return(
+        <>
+            <div className={styles.spinner}>
+                <div className={styles.doubleBounce1}></div>
+                <div className={styles.doubleBounce2}></div>
+            </div>
+        </>
+    );
+};
+
+export default Loading;
--- src/containers/Latest/Latest.js
+++ src/containers/Latest/Latest.js
@@ -3,10 +3,11 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import Comic from '../../components/Comic/Comic';
 import style from '../../styles/global.module.scss';
+import Loading from '../../components/Loading/Loading';
 
 function Latest({comic}) {
     if (!comic) {
-        return (<div>Loading...</div>);
+        return (<Loading/>);
     }
     return (
         <section className={`${style.container} ${style['text-center']}`}>
