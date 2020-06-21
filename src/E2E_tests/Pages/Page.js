e72bbfd8e6a40bc51d7622d88f2705cb7b2e5be5
Carlos Castillo <info@carloscastillo.cl>
2020-06-21 19:56:40 +0100

    Refactoring Testcafe

--- src/E2E_tests/Pages/Page.js
+++ src/E2E_tests/Pages/Page.js
@@ -1,50 +1,63 @@
-import { Selector, t } from 'testcafe';
+import { ReactSelector, t } from 'testcafe-react-selectors';
+
 
 class Page {
     constructor() {
-        this.comicTitle = Selector('[data-testid=comic-title]');
-        this.comicNumber = Selector('[data-testid=comic-number]');
-        this.comicImage = Selector('[data-testid=comic-image]');
-        this.comicAlt = Selector('[data-testid=comic-alt]');
-        this.toogleSearchBtn = Selector('[data-testid=toogle-search-btn]');
-        this.searchComicInput = Selector('[data-testid=search-comic-input]');
+        this.comic = ReactSelector('Comic');
+        this.header = ReactSelector('Header');
+        this.comicTitle = this.comic.find('[data-testid=comic-title]');
+        this.comicNumber = this.comic.find('[data-testid=comic-number]');
+        this.comicImage = this.comic.find('[data-testid=comic-image]');
+        this.comicAlt = this.comic.find('[data-testid=comic-alt]');
+        this.toogleSearchBtn = this.header.find('#toogle-search-btn');
+        this.searchComicInput = this.header.find('[data-testid=search-comic-input]');
+        this.searchBtn = this.header.find('#searchBarBtn');
+
     }
 
     async checkComicTitle() {
-        const comicTitle = this.comicTitle;
+        const comicTitle = await this.comicTitle.getReact();
         await t
             .expect(comicTitle).ok();
     }
 
     async checkComicNumber() {
-        const comicNumber = this.comicNumber;
+        const comicNumber = await this.comicNumber.getReact();
         await t
             .expect(comicNumber).ok();
     }
 
     async checkComicImage() {
-        const comicImage = this.comicImage;
+        const comicImage = await this.comicImage.getReact();
         await t
             .expect(comicImage).ok();
     }
 
     async checkComicAlt() {
-        const comicAlt = this.comicAlt;
+        const comicAlt = await this.comicAlt.getReact();
         await t
             .expect(comicAlt).ok();
     }
 
     async toogleSearch() {
-        const toogleSearchBtn = this.toogleSearchBtn;
-        const searchComicInput = this.searchComicInput;
+        const toogleSearchBtn = await this.toogleSearchBtn.getReact();
+        const searchComicInput = await this.searchComicInput.getReact();
+        const searchBtn = await this.searchBtn.getReact();
         await t
-            .expect(toogleSearchBtn).ok();
-        await t
+            .expect(toogleSearchBtn)
+            .ok()
             .click(toogleSearchBtn);
+
         await t
-            .expect(searchComicInput).ok();
+            .expect(searchComicInput)
+            .ok();
         await t
             .typeText(searchComicInput, '123', {replace: true});
+
+        await t
+            .expect(searchComicInput)
+            .ok()
+            .click(searchBtn);
 
     }
 
--- src/E2E_tests/e2e.js
+++ src/E2E_tests/e2e.js
@@ -1,11 +1,15 @@
 import config from './configuration';
+import { waitForReact } from 'testcafe-react-selectors';
+
 import { userCheckLatestComic } from './Journeys/userCheckLatestComic';
 import {userSearch} from './Stories/searchBar';
 // Pages > Stories > Journeys
 
-fixture
-    .disablePageCaching `XKXC test`
-    .page(config.url);
+fixture `XKXC test`
+    .page(config.url)
+    .beforeEach(async t => {
+        await waitForReact(5000, t);
+    });
 
 test('User check Latest comic', async () => {
     userCheckLatestComic();
