079fc8f31edc699df030dc9df18b680876bb55d5
Carlos Castillo <info@carloscastillo.cl>
2020-06-21 15:32:22 +0100

    Updates

---
+++ precache-manifest.c60205fe62d0aaaec0237d3181fb2433.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59df60b91080e89e2db37313497e7444",
+    "url": "/XKXC-comic-search/index.html"
+  },
+  {
+    "revision": "d713248e51fe83f80141",
+    "url": "/XKXC-comic-search/static/css/2.9905d55a.chunk.css"
+  },
+  {
+    "revision": "b3780233d0cca7bb7664",
+    "url": "/XKXC-comic-search/static/css/main.ef031634.chunk.css"
+  },
+  {
+    "revision": "d713248e51fe83f80141",
+    "url": "/XKXC-comic-search/static/js/2.60b0854f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/XKXC-comic-search/static/js/2.60b0854f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3780233d0cca7bb7664",
+    "url": "/XKXC-comic-search/static/js/main.1a4c5bc5.chunk.js"
+  },
+  {
+    "revision": "152ffa10cc3a9a8bd9bd",
+    "url": "/XKXC-comic-search/static/js/runtime-main.cdd6f23c.js"
+  },
+  {
+    "revision": "78f486ee61def5b6d8d730ae3ad4d0cf",
+    "url": "/XKXC-comic-search/static/media/logo_2.78f486ee.png"
+  }
+]);
